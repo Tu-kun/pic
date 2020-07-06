5160a87957cc7cef4c60e649f7d534b943a15d12
Twain-kun <2018605007@qq.com>
2020-07-06 18:18:04 +0800

    获取年月日的情况1  20160312

--- test.py
+++ test.py
@@ -11,10 +11,15 @@ def get_year(title):
     :param title: 照片标题
     :return: 返回日期
     """
-    year = re.search(r'\d{4}', repr(title))
-    month = re.search(r'')
+    try:
+        create_date = re.search(r'\d{4}\d{2}\d{2}', title)
+        year = create_date.group(0)[:4]  #获取年份
+        month = create_date.group(0)[4:6]
+        day = create_date.group(0)[6:8]
+    except Exception:
+        print("Error")
     # print(year.group(0))
-    return year.group(0)
+    return year, month, day
 
 
 def get_name(title):
@@ -40,7 +45,7 @@ def read(path):
             number =  line[-12::].strip('\n')  #图片编号
             title_list  = line[:-18:].split('\\')   #将有效信息分割为列表，如['Z:', 'yuexun', '2018图编外拍 2', '炫彩世界收图-3124张', '周世杰-20181025炫彩世界开幕式', 'DSCF4066.JPG']
             tag_1 = title_list[2]  #一级标签，作品来源
-            year = get_year(title_list)  #年份
+            year, month, day = get_year(title_list)  #获取年月日
             name = get_name(line)  #作者
             # print(title_list)
             pic[number] = [tag_1, year, name]
